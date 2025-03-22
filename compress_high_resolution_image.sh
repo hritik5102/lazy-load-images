743481ae3fba4396bcec57e072d334b1f7d2c713
hritik5102 <hritik.dj2000@gmail.com>
2025-03-22 14:30:15 +0530

    Update compress high resolution image script 🔖

--- compress_high_resolution_image.sh
+++ compress_high_resolution_image.sh
@@ -2,19 +2,30 @@
 
 echo "Compress high resolution images"
 
-num_images=16
+num_images=18
 
-for((i=1; i<=num_images;i++))
+for ((i=1; i<=num_images; i++))
 do
     input_image="images/high-resolution-images/image-$i.jpg"
-    output_image="images/webp-images/image-$i.jpg"
+    output_folder="images/compressed-high-resolution-images/"
+    output_image="images/compressed-high-resolution-images/image-$i.jpg"
 
-    ffmpeg -i "$input_image" -quality 95 "$output_image"
+    # Check if the folder exists
+    if [ ! -d "$output_folder" ]; then
+        # If the folder does not exist, create it
+        mkdir -p "$output_folder"
+        echo "Folder created: $output_folder"
+    else
+        echo "Folder already exists: $output_folder"
+    fi
 
+    # Compress the image using ffmpeg
+    ffmpeg -i "$input_image" -q:v 15 "$output_image"
+
+    # Check if the ffmpeg command was successful
     if [ $? -eq 0 ]; then
         echo "Converted $input_image to $output_image successfully."
     else
         echo "Failed to resize $input_image"
     fi
-
 done
